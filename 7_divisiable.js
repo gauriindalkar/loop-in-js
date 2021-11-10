fc5e747c85680ee97632fc2798c6dbfdc1e523a4
gauriindalkar <84510274+gauriindalkar@users.noreply.github.com>
2021-11-10 16:34:04 +0530

    Add files via upload

---
+++ 7_divisiable.js
@@ -0,0 +1,23 @@
+// var a = 1;
+// while(a<100){
+//     if (a%7==0){
+//         cons"ole.log(a)
+//     }
+//     a++
+// }
+
+// for(var a = 1;(a<100);a++){
+//     if (a%7==0){
+//         console.log(a)
+//     }
+// }
+
+// var a = 1
+// do{
+//     if (a%7==0){
+//         console.log(a)
+//     }
+//     a++
+// }
+// while(a<100)
+
---
+++ navgurukul_print.js
@@ -0,0 +1,27 @@
+// var i=1
+// while(i<=100){
+//     if (i%3==0){
+//         console.log("nav")
+//     }else if(i%7==0){
+//         console.log("gurukul")
+//     }else if (i%3==0 && i%7==0){
+//         console.log("navgurukul")
+//     }else{
+//         console.log(i)
+//     }
+// }
+
+// var i = 1;
+// do{
+//     if (i%3==0 && i%7==0){
+//         console.log("nav")
+//     }else if(i%7==0){
+//         console.log("gurukul")
+//     }else if (i%3==0){
+//         console.log("nav")
+//     }else{
+//         console.log(i)
+//     }
+//     i++
+// }
+// while(i<=100)
